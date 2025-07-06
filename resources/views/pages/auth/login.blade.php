debcda5ed20d99066f0806a3af638bd8580ef153
Danang Andre Wijaya <danangandrewijaya@gmail.com>
2025-07-06 20:25:18 +0700

    update login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -13,14 +13,16 @@
 
             <!--begin::Subtitle-->
             <div class="text-gray-500 fw-semibold fs-6">
-                Your Social Campaigns
+                User Dosen<br>
+                >Email: NIP + @undip.ac.id<br>
+                >Password: Tgl Lahir (DDMMYYYY)
             </div>
             <!--end::Subtitle--->
         </div>
         <!--begin::Heading-->
 
         <!--begin::Login options-->
-        <div class="row g-3 mb-9">
+        {{-- <div class="row g-3 mb-9">
             <!--begin::Col-->
             <div class="col-md-6">
                 <!--begin::Google link--->
@@ -43,13 +45,13 @@
                 <!--end::Google link--->
             </div>
             <!--end::Col-->
-        </div>
+        </div> --}}
         <!--end::Login options-->
 
         <!--begin::Separator-->
-        <div class="separator separator-content my-14">
+        {{-- <div class="separator separator-content my-14">
             <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
-        </div>
+        </div> --}}
         <!--end::Separator-->
 
         <!--begin::Input group--->
@@ -72,9 +74,9 @@
             <div></div>
 
             <!--begin::Link-->
-            <a href="{{ route('password.request') }}" class="link-primary">
+            {{-- <a href="{{ route('password.request') }}" class="link-primary">
                 Forgot Password ?
-            </a>
+            </a> --}}
             <!--end::Link-->
         </div>
         <!--end::Wrapper-->
@@ -88,13 +90,13 @@
         <!--end::Submit button-->
 
         <!--begin::Sign up-->
-        <div class="text-gray-500 text-center fw-semibold fs-6">
+        {{-- <div class="text-gray-500 text-center fw-semibold fs-6">
             Not a Member yet?
 
             <a href="{{ route('register') }}" class="link-primary">
                 Sign up
             </a>
-        </div>
+        </div> --}}
         <!--end::Sign up-->
     </form>
     <!--end::Form-->
