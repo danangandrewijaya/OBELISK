496471a7f225ed35f21a06290df8f5c2cd1d1f6c
Danang Andre Wijaya <danangandrewijaya@gmail.com>
2025-07-06 23:03:02 +0700

    update view

--- resources/views/partials/menus/_user-account-menu.blade.php
+++ resources/views/partials/menus/_user-account-menu.blade.php
@@ -1,5 +1,5 @@
 <!--begin::User account menu-->
-<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
+<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-300px" data-kt-menu="true">
     <!--begin::Menu item-->
     <div class="menu-item px-3">
         <div class="menu-content d-flex align-items-center px-3">
@@ -17,7 +17,7 @@
             <!--begin::Username-->
             <div class="d-flex flex-column">
                 <div class="fw-bold d-flex align-items-center fs-5">{{ Auth::user()->name}}
-                    {{-- <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span> --}}
+                    <span class="badge badge-light-{{ session('active_role') ? 'primary' : 'warning' }} fw-bold fs-8 px-2 py-1 ms-2">{{ session('active_role') ?? 'Role belum dipilih' }}</span>
                 </div>
                 @php
                     $email = '-';
