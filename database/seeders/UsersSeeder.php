389971c00bd96654b2fc53252e1c3b7e13c6927c
Danang Andre Wijaya <danangandrewijaya@gmail.com>
2025-07-06 14:47:56 +0700

    user seeder role dosen

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Dosen;
 use App\Models\User;
 use Faker\Generator;
 use Illuminate\Database\Seeder;
@@ -14,20 +15,37 @@ class UsersSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Generator $faker)
-    {
-        $demoUser = User::create([
-            'name'              => $faker->name,
-            'email'             => 'demo@demo.com',
-            'password'          => Hash::make('demo'),
-            'email_verified_at' => now(),
-        ]);
+    // public function run(Generator $faker)
+    // {
+    //     $demoUser = User::create([
+    //         'name'              => $faker->name,
+    //         'email'             => 'demo@demo.com',
+    //         'password'          => Hash::make('demo'),
+    //         'email_verified_at' => now(),
+    //     ]);
 
-        $demoUser2 = User::create([
-            'name'              => $faker->name,
-            'email'             => 'admin@demo.com',
-            'password'          => Hash::make('demo'),
-            'email_verified_at' => now(),
-        ]);
+    //     $demoUser2 = User::create([
+    //         'name'              => $faker->name,
+    //         'email'             => 'admin@demo.com',
+    //         'password'          => Hash::make('demo'),
+    //         'email_verified_at' => now(),
+    //     ]);
+    // }
+
+    public function run()
+    {
+        $dosens = Dosen::all();
+        // dd($dosens);
+
+        foreach($dosens as $dosen){
+            $user = User::create([
+                'name'              => $dosen->nama,
+                'email'             => $dosen->nip . '@undip.ac.id',
+                'password'          => Hash::make($dosen->tgl),
+                'email_verified_at' => now(),
+            ]);
+
+            User::find($user->id)->assignRole('dosen');
+        }
     }
 }
